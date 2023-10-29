0f8827fea6414c1a3c7ea287f79d587df352b3d1
Pisit Chingulprasan <66056059@kmitl.ac.th>
2023-10-29 19:39:32 +0700

    Add comment and test run

--- stl_app/04_penguin_ml/app_st.py
+++ stl_app/04_penguin_ml/app_st.py
@@ -15,7 +15,7 @@ st.write("This app uses 6 inputs to predict the species of penguin using "
 penguin_file = st.file_uploader('Upload your own penguin data')
 
 if penguin_file is None:
-    rf_pickle = open('random_forest_penguin.pickle', 'rb')
+    rf_pickle = open('random_forest_penguin.pickle', 'rb')  # > Read-binary
     map_pickle = open('output_penguin.pickle', 'rb')
 
     rfc = pickle.load(rf_pickle)
