ebc81b20b5922b9e6822dacb3eb8407c216335e9
Pisit Chingulprasan <66056059@kmitl.ac.th>
2023-10-29 19:34:12 +0700

    Add comment and test run

--- stl_app/04_penguin_ml/app_ml.py
+++ stl_app/04_penguin_ml/app_ml.py
@@ -10,21 +10,21 @@ from sklearn.model_selection import train_test_split
 import pickle
 
 penguin_df = pd.read_csv('penguins.csv')
-penguin_df.dropna(inplace=True)
-output = penguin_df['species']
+penguin_df.dropna(inplace=True)  # drop not available
+output = penguin_df['species']  # >> Supervised learning
 features = penguin_df[['island', 'bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g', 'sex']]
 features = pd.get_dummies(features)
 output, uniques = pd.factorize(output)
 
 x_train, x_test, y_train, y_test = train_test_split(
-    features, output, test_size=.8)
+    features, output, test_size=.8)  # >> data 80% to test
 rfc = RandomForestClassifier(random_state=15)
 rfc.fit(x_train, y_train)
 y_pred = rfc.predict(x_test)
 score = accuracy_score(y_pred, y_test)
 print('Our accuracy score for this model is {}'.format(score))
 
-rf_pickle = open('random_forest_penguin.pickle', 'wb')
+rf_pickle = open('random_forest_penguin.pickle', 'wb')  # >> write-binary
 pickle.dump(rfc, rf_pickle)
 rf_pickle.close()
 output_pickle = open('output_penguin.pickle', 'wb')
